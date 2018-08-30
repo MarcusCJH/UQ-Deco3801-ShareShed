af5495cf7bd31e2abe2bdb1a60006f8bb47821b7
Risyad Hasbullah <r.hasbullah@uqconnect.edu.au>
2018-08-30 21:43:52 +1000

    Hide secret key, configure mysql database

--- share_shed/settings.py
+++ share_shed/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'na(z5zxt)+p%lexv$q5myi_up!vae+cf#ifvwz#r)6ok60tt%z'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'share_shed.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST' : os.environ.get('DB_HOST'),
     }
 }
 
