b9f720f7e6b34297241bbdf8c2b6bbf346e79587
Marcus <marcuschanjh@gmail.com>
2018-08-31 21:28:45 +1000

    Add in Contact Table Update Maintenance Table

--- main_website/models.py
+++ main_website/models.py
@@ -35,12 +35,36 @@ class Membership(models.Model):
     Paid = models.BooleanField(default=False)
 
 class Maintenance(models.Model):
+
     MAINTENANCE_OPTIONS = (
         ('0', 'Ok'),
         ('1', 'At Repairer'),
         ('2', 'With Staff Member'),
     )
     Maintenance_id = models.AutoField(primary_key=True)
+    Maintenance_status = models.CharField(choices=MAINTENANCE_OPTIONS, max_length=1)
     Maintenance_location = models.CharField(max_length=255)
     Maintenance_notes = models.TextField(blank=True, null=True)
+
+class Contact(models.Model):
+    LANGUAGE_OPTIONS =(
+        ('0', 'English'),
+    )
+    Contact_id = models.AutoField(primary_key=True)
+    First_name = models.CharField(max_length=30)
+    Last_name = models.CharField(max_length=30)
+    Address = models.CharField(max_length=200)
+    Email = models.EmailField(max_length=70, null=True, blank=True, unique=True)
+    Telephone_num = models.IntegerField()
+    Postcode = models.IntegerField()
+    City = models.CharField(max_length=20)
+    County = models.CharField(max_length=50)
+    Preferred_language = models.CharField(choices=LANGUAGE_OPTIONS, max_length=1)
+    Account_balance = models.FloatField()
+    Maillist = models.BooleanField(default=False)
+    Password = models.CharField(max_length=128)
+    Membership_num = models.ForeignKey(Membership, on_delete=models.CASCADE)
+
+
+
 
