d9f0a6e3b597bd63ca688c7352be9801938146c4
Marcus <marcuschanjh@gmail.com>
2018-08-31 20:55:25 +1000

    Include Membership Table and Maintenance Table

--- main_website/models.py
+++ main_website/models.py
@@ -20,3 +20,27 @@ class Semester(models.Model):
     year = models.IntegerField()
     semester_type = models.CharField(choices=semester_options, max_length=1)
     courses = models.ManyToManyField(Course)
+
+
+class Membership(models.Model):
+    MEMBERSHIP_OPTIONS = (
+        ('0', 'Regular'),
+        ('1', 'Cancelled'),
+        ('2', 'Admin'),
+    )
+    Membership_num = models.AutoField(primary_key=True)
+    Membership_type = models.CharField(choices=MEMBERSHIP_OPTIONS, max_length=1)
+    Start_date = models.DateField(blank=True,null=True)
+    End_date = models.DateField(blank=True,null=True)
+    Paid = models.BooleanField(default=False)
+
+class Maintenance(models.Model):
+    MAINTENANCE_OPTIONS = (
+        ('0', 'Ok'),
+        ('1', 'At Repairer'),
+        ('2', 'With Staff Member'),
+    )
+    Maintenance_id = models.AutoField(primary_key=True)
+    Maintenance_location = models.CharField(max_length=255)
+    Maintenance_notes = models.TextField(blank=True, null=True)
+
