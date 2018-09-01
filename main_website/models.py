cde54a5ceab5edb93ba67b52d1ededc75834d126
Risyad Hasbullah <r.hasbullah@uqconnect.edu.au>
2018-09-01 14:25:09 +1000

    Remove previous example table

--- main_website/models.py
+++ main_website/models.py
@@ -2,26 +2,6 @@ from django.db import models
 from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 # Create your models here.
-class Course(models.Model):
-    name = models.CharField(max_length=15)
-    code = models.CharField(max_length=10, primary_key=True)
-
-class Student(models.Model):
-    id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-
-class Semester(models.Model):
-    semester_options=(
-        ('1', 'Odd'),
-        ('2', 'Even'),
-    )
-    id = models.AutoField(primary_key=True)
-    year = models.IntegerField()
-    semester_type = models.CharField(choices=semester_options, max_length=1)
-    courses = models.ManyToManyField(Course)
-
-
 class Membership(models.Model):
     MEMBERSHIP_OPTIONS = (
         ('0', 'Regular'),
@@ -64,7 +44,3 @@ class Contact(models.Model):
     Maillist = models.BooleanField(default=False)
     Password = models.CharField(max_length=128)
     Membership_num = models.ForeignKey(Membership, on_delete=models.CASCADE)
-
-
-
-
