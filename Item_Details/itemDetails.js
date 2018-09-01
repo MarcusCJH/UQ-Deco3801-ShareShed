a430b898b33790f76b6da6a323e998d93f99a365
Shao Qi Tan <tansq16@gmail.com>
2018-09-01 16:28:54 +1000

    Edits to item detail page

--- Item_Details/itemDetails.js
+++ Item_Details/itemDetails.js
@@ -1,14 +1,14 @@
 $('.carousel').carousel()
 
 $(function () {
-        $('#datetimepicker6').datetimepicker();
-        $('#datetimepicker7').datetimepicker({
+        $('#datetimepicker1').datetimepicker();
+        $('#datetimepicker2').datetimepicker({
             useCurrent: false //Important! See issue #1075
         });
-        $("#datetimepicker6").on("dp.change", function (e) {
-            $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+        $("#datetimepicker1").on("dp.change", function (e) {
+            $('#datetimepicker1').data("DateTimePicker").minDate(e.date);
         });
-        $("#datetimepicker7").on("dp.change", function (e) {
-            $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+        $("#datetimepicker2").on("dp.change", function (e) {
+            $('#datetimepicker2').data("DateTimePicker").maxDate(e.date);
         });
     });
