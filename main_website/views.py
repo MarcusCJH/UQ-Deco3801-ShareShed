20366baf8557ac43930215609c70009640d27b2b
Risyad Hasbullah <r.hasbullah@uqconnect.edu.au>
2018-09-05 00:14:53 +1000

    Add sample stripe payment

--- main_website/views.py
+++ main_website/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.conf import settings
+import stripe
 
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 # Create your views here.
 def index(request):
@@ -13,7 +16,12 @@ def louis(request, random_number):
 def risyad(request, random_number):
     return HttpResponse("Hello, world. " + str(random_number))
 
-
 def paymenttest(request):
-    return render(request, 'paymentsystest/test.html')
-
+    charge = stripe.Charge.create(
+      amount=2000,
+      currency="aud",
+      source="tok_mastercard", # obtained with Stripe.js
+      metadata={'order_id': '6735'}
+    )
+    print(charge)
+    return HttpResponse(charge)
