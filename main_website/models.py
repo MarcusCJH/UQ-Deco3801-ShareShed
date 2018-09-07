84b45dac454ee3b17442534c8591ac8514864ec5
s4511443 <louis.kakioshe@gmail.com>
2018-09-07 15:15:23 +1000

    create user models

--- main_website/models.py
+++ main_website/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinLengthValidator, MaxLengthValidator
+import datetime
 
 # Create your models here.
 
@@ -11,26 +12,30 @@ class Member(models.Model):
         ('l', 'Librarian'),
     )
     membership = models.CharField(choices=membership_options, max_length=1, default='g')
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+    start_time = models.DateTimeField('Start',default=datetime.datetime.now)
+    end_time = models.DateTimeField('End',default=datetime.datetime.now)
+
+    @classmethod
+    def get_new(cls):
+        return cls.objects.create().id
 
 class User(models.Model):
     contact_id = models.AutoField(primary_key=True)
-    member_id = models.IntegerField(unique=True)
     added_on = models.DateTimeField(auto_now_add=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    membership = models.ForeignKey(
-        'Member',
+    membership = models.OneToOneField(
+        Member,
         on_delete=models.CASCADE,
+        default=Member.get_new,
     )
     email = models.EmailField(unique=True)
     subscriber = models.BooleanField(default=True)
     telephone = models.CharField(max_length=15)
-    street = models.TextField()
+    street = models.TextField(max_length=30)
     city = models.CharField(max_length=30)
     county = models.CharField(max_length=30)
     postcode = models.CharField(max_length=4)
     country = models.CharField(max_length=30)
-    balance = models.FloatField()
+    balance = models.FloatField(default=0)
     suburb = models.CharField(max_length=30)
