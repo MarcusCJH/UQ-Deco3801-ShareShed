18693407d59c4b9424e78cc80c8b916a286f272f
Risyad Hasbullah <r.hasbullah@uqconnect.edu.au>
2018-09-10 01:29:06 +1000

    Finished product and cart

--- main_website/admin.py
+++ main_website/admin.py
@@ -1,16 +1,43 @@
 from django.contrib import admin
-from .models import Course, Student, Semester
+from .models import Product, ProductImage, ProductType, ProductTags, \
+    ProductLocation, ProductCondition, Cart
 
 # Register your models here.
-class CourseAdmin(admin.ModelAdmin):
-     list_display = ('name', 'code')
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'loan_period')
 
-class StudentAdmin(admin.ModelAdmin):
-     list_display = ('first_name', 'last_name')
 
-class SemesterAdmin(admin.ModelAdmin):
-     list_display = ('year', 'semester_type')
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ('product', 'alt')
 
-admin.site.register(Course, CourseAdmin)
-admin.site.register(Student, StudentAdmin)
-admin.site.register(Semester, SemesterAdmin)
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    list_display = ('type_name',)
+
+
+class ProductTagsAdmin(admin.ModelAdmin):
+    list_display = ('tag_name',)
+
+
+class ProductLocationAdmin(admin.ModelAdmin):
+    list_display = ('location_name',)
+
+
+class ProductConditionAdmin(admin.ModelAdmin):
+    list_display = ('condition_name',)
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'item')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductImage, ProductImageAdmin)
+admin.site.register(ProductType, ProductTypeAdmin)
+admin.site.register(ProductTags, ProductTagsAdmin)
+admin.site.register(ProductLocation, ProductLocationAdmin)
+admin.site.register(ProductCondition, ProductConditionAdmin)
+admin.site.register(Cart, CartAdmin)
+admin.site.site_header = "Share Shed Admin"
+admin.site.site_title = "Share Shed admin login"
+admin.site.index_title = "Hello"
--- main_website/models.py
+++ main_website/models.py
@@ -1,22 +1,79 @@
 from django.db import models
-from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.conf import settings
 
 # Create your models here.
-class Course(models.Model):
-    name = models.CharField(max_length=15)
-    code = models.CharField(max_length=10, primary_key=True)
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    long_description = models.TextField()
+    short_description = models.CharField(max_length=50)
+    fee = models.DecimalField(max_digits=8, decimal_places=2)
+    available_on = models.DateField(blank=True, null=True)
+    shown = models.BooleanField(default=True)
+    added_on = models.DateField(blank=True, null=True, auto_now_add=True)
+    code = models.CharField(max_length=8)
+    brand = models.CharField(max_length=32)
+    price_paid = models.DecimalField(max_digits=8, decimal_places=2)
+    value = models.DecimalField(max_digits=8, decimal_places=2)
+    loan_period = models.IntegerField()
+    components = models.TextField()
+    care_information = models.TextField()
+    keywords = models.CharField(max_length=128)
+    type = models.ForeignKey('ProductType',
+        null=True, on_delete=models.SET_NULL)
+    tags = models.ForeignKey('ProductTags',
+        null=True, on_delete=models.SET_NULL)
+    location = models.ForeignKey('ProductLocation',
+        null=True, on_delete=models.SET_NULL)
+    condition = models.ForeignKey('ProductCondition',
+        null=True, on_delete=models.SET_NULL)
 
-class Student(models.Model):
-    id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    def __str__(self):
+        return str(self.name)
 
-class Semester(models.Model):
-    semester_options=(
-        ('1', 'Odd'),
-        ('2', 'Even'),
-    )
-    id = models.AutoField(primary_key=True)
-    year = models.IntegerField()
-    semester_type = models.CharField(choices=semester_options, max_length=1)
-    courses = models.ManyToManyField(Course)
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name='images',
+        on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products', blank=False)
+    alt = models.CharField(max_length=128, blank=True)
+
+    def __str__(self):
+        return str(self.alt)
+
+
+class ProductType(models.Model):
+    type_name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return str(self.type_name)
+
+
+class ProductTags(models.Model):
+    tag_name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return str(self.tag_name)
+
+
+class ProductCondition(models.Model):
+    condition_name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return str(self.condition_name)
+
+
+class ProductLocation(models.Model):
+    location_name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return str(self.location_name)
+
+
+class Cart(models.Model):
+    item = models.ForeignKey('Product',
+        null=False, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL,
+        null=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.item)
