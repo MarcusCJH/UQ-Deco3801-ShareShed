6ad95f51bec210a3c9f4c2058cb724b7d34be885
Risyad Hasbullah <r.hasbullah@uqconnect.edu.au>
2018-09-11 14:29:59 +1000

    Add media url, add inline product image

--- main_website/admin.py
+++ main_website/admin.py
@@ -1,14 +1,30 @@
 from django.contrib import admin
 from .models import Product, ProductImage, ProductType, ProductTags, \
     ProductLocation, ProductCondition, Cart
+from django.utils.html import mark_safe
 
 # Register your models here.
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    readonly_fields = ('image_tag',)
+
+    def image_tag(self, obj):
+        width='200px'
+        height='200px'
+        return mark_safe('<img src="{}" width={} height={}/>'.format(obj.image.url, width, height))
+
+
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'code', 'loan_period')
+    list_display = ('name', 'code', 'loan_period',)
+    inlines = [ProductImageInline]
 
 
 class ProductImageAdmin(admin.ModelAdmin):
     list_display = ('product', 'alt')
+    readonly_fields = ('image_tag',)
+
+    def image_tag(self, obj):
+        return mark_safe('<img src="{}" />'.format(obj.image.url))
 
 
 class ProductTypeAdmin(admin.ModelAdmin):
