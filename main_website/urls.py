5ec74fa3a4b65b5831d5c198256425d05bfaecc8
Marcus <marcuschanjh@gmail.com>
2018-09-11 23:01:19 +1000

    Getting to the subscription.
    Change some views and URL

--- main_website/urls.py
+++ main_website/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('index/', views.index, name='homepage'),
     path('testing/<int:random_number>', views.louis, name='dynamic_web'),
 
-    path('paymenttest/', views.paymenttest, name='Payment Test'),
+    path('checkout/', views.checkout, name='Payment Test'),
 ]
--- main_website/views.py
+++ main_website/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.conf import settings
+from django.contrib import messages
 import stripe
+from django.views.decorators.csrf import csrf_exempt
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -16,7 +18,20 @@ def louis(request, random_number):
 def risyad(request, random_number):
     return HttpResponse("Hello, world. " + str(random_number))
 
-def paymenttest(request):
+@csrf_exempt
+def checkout(request):
+    if request.method == "POST":
+        token = request.POST['stripeToken']
+        try:
+            charge = stripe.Charge.create(
+                amount=999,
+                currency="aud",
+                source=token,
+                description="The product charged to the user"
+            )
+            messages.info(request, "Payment is successful")
+        except stripe.error.CardError as ce:
+            return False, ce
 
     context = {"stripe_key": settings.STRIPE_PUBLISHABLE_KEY}
     return render(request,'paymentsystest/test.html',context)
