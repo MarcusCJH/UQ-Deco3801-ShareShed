3fe62a3210cfc5fa3aef4f3290e0efd85f7bee19
s4511443 <louis.kakioshe@gmail.com>
2018-09-14 13:04:41 +1000

    add membership system

--- main_website/urls.py
+++ main_website/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^signup/$', views.SignUp , name='signup'),
-
+    url(r'^membership/$',views.membershipRenew, name='membership')
 ]
--- main_website/views.py
+++ main_website/views.py
@@ -1,5 +1,10 @@
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from dateutil.relativedelta import relativedelta
+import datetime
+
+from .models import User, Member
 
 from .forms import UserCreationForm
 
@@ -16,3 +21,16 @@ def SignUp(request):
     else:
         form = UserCreationForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+def membershipRenew(request):
+    current_user = request.user
+    if request.method == 'POST' :
+        member = Member.objects.get(user_id=current_user.id)
+        if member.start_time == None and member.end_time == None:
+            member.membership_type = "m"
+            member.start_time = datetime.datetime.now()
+            member.end_time = member.start_time + relativedelta(years=1)
+        elif (member.end_time):
+            member.end_time = member.end_time + relativedelta(years=1)
+        member.save()
+    return redirect('home')
