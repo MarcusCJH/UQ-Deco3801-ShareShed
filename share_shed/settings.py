8c0e3839fbdae9a55dce09c07833e24f417551fd
Marcus <marcuschanjh@gmail.com>
2018-09-16 20:46:27 +1000

    Include Email Settings in setting.py

--- share_shed/settings.py
+++ share_shed/settings.py
@@ -26,6 +26,14 @@ STRIPE_PUBLISHABLE_KEY = os.environ.get('STRIPE_PUBLIC')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+#EMAIL SETTINGS
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
