ba43679195a3403a9f065716c011b4d2f90bd818
Marcus <marcuschanjh@gmail.com>
2018-09-20 01:05:52 +1000

    Include URL for Topup

--- main_website/urls.py
+++ main_website/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^signup/$', views.SignUp , name='signup'),
     url(r'^membership/$',views.membershipRenew, name='membership'),
-    url(r'^topup/$',views.topupCredit, name='topup')
+    url(r'^topup/$',views.topupCredit, name='topup'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
