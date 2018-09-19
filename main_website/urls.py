77dc54269ba3688cc15efb84719b13fd20a954b6
Marcus <marcuschanjh@gmail.com>
2018-09-19 23:18:39 +1000

    Got the front end to custom amount. Bad looking button

--- main_website/urls.py
+++ main_website/urls.py
@@ -8,5 +8,6 @@ from django.conf import settings
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^signup/$', views.SignUp , name='signup'),
-    url(r'^membership/$',views.membershipRenew, name='membership')
+    url(r'^membership/$',views.membershipRenew, name='membership'),
+    url(r'^topup/$',views.topupCredit, name='topup')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
