0dd3f346c72dfeab800e5aa5473533020ce3b470
s4511443 <louis.kakioshe@gmail.com>
2018-09-23 22:12:28 +1000

    base for email auth

--- main_website/urls.py
+++ main_website/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import url
 from django.views.generic.base import TemplateView
 from . import views
 from django.conf.urls.static import static
@@ -10,5 +11,8 @@ urlpatterns = [
     path('membership',views.membership_renew, name='membership'),
     path('topup',views.top_up_credit, name='topup'),
     path('profile', TemplateView.as_view(template_name='user/profile.html'),
-          name='profile')
+          name='profile'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.user_activation, name='activate'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main_website/views.py
+++ main_website/views.py
@@ -1,11 +1,19 @@
 import datetime
-from django.core.mail import send_mail
-from django.shortcuts import render, redirect, get_object_or_404
+
+from django.core.mail import send_mail, EmailMessage
+from django.contrib.sites.shortcuts import get_current_site
 from django.conf import settings
 from django.contrib.auth import login, authenticate
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.shortcuts import render, redirect, get_object_or_404
 from dateutil.relativedelta import relativedelta
 from django.utils import timezone
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.views.decorators.csrf import csrf_exempt
+
+from .tokens import account_activation_token
 from .models import User, Member, Payment
 from .forms import UserCreationForm
 import stripe
@@ -17,16 +25,40 @@ def sign_up(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            email = form.cleaned_data.get('email')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(email=email, password=raw_password)
-            login(request, user)
-
-            return redirect('home')
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Share Shed Email Activation'
+            message = render_to_string('registration/email_activation.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)).decode(),
+                'token':account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
     else:
         form = UserCreationForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+def user_activation(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return redirect('home')
+    else:
+        return HttpResponse('Activation link is invalid!')
 
 
 def membership_renew(request):
