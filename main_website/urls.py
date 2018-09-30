7f1f65e2d25f9389c897a5a0b62a6dd10859d335
kakioshe <louis.kakioshe@gmail.com>
2018-09-30 14:53:36 +1000

    added forgot password

--- main_website/urls.py
+++ main_website/urls.py
@@ -3,6 +3,7 @@ from django.views.generic.base import TemplateView
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
