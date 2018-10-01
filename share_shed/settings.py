b4caec1a7eae23ab689c49e7deb92a340ba20166
Risyad Hasbullah <r.hasbullah@uqconnect.edu.au>
2018-10-01 14:26:45 +1000

    Change email settings to gmail

--- share_shed/settings.py
+++ share_shed/settings.py
@@ -31,12 +31,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 #EMAIL SETTINGS
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = os.environ.get('EMAIL_HOST_PORT')
-EMAIL_USE_SSL = True
+EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
