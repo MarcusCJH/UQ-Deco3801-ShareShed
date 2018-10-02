1c2dbe7f37b06aa707963089407832f6818ebaeb
Marcus <marcuschanjh@gmail.com>
2018-10-02 22:37:30 +1000

    Add on catalogue files

--- main_website/urls.py
+++ main_website/urls.py
@@ -7,6 +7,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('catalogue/', TemplateView.as_view(template_name='catalogue.html'), name='catalogue'),
     path('signup/', views.sign_up , name='signup'),
     path('membership',views.membership_renew, name='membership'),
     path('topup',views.top_up_credit, name='topup'),
